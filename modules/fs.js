9be1027a25cd4af04ae8ade884194b26cc4f3e33
Botirbek2702 <userbotirbek2702@gmail.com>
2024-01-08 16:56:28 +0500

    newcode

--- modules/fs.js
+++ modules/fs.js
@@ -1,3 +1,29 @@
 const fs = require("fs")
 const path = require("path")
-console.log("1");
+
+
+// fs.mkdir(path.join(__dirname, "templetse") , err =>{
+//     if(err) throw new Error()
+
+//     console.log("sucsesflly");
+// })
+
+fs.writeFile(path.join(__dirname, "templetse" , "Janury.txt") , "Create new course Nodejs" , err => {
+    if(err) throw new Error()
+
+
+    console.log("create");
+
+    fs.appendFile(path.join(__dirname, "templetse" , "Janury.txt") ," and Reactjs", err=>{
+         if(err) throw new Error()
+
+         console.log("sucsesfully");
+    })
+    
+    fs.readFile(path.join(__dirname, "templetse", "Janury.txt"), (err, data)=>{
+        if(err) throw new Error()
+
+        console.log(Buffer.from(data).toString());
+    })
+})
+
