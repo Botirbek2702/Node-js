52ddaecc027be8f2a75419b87ccfc2736fe0b156
Botirbek2702 <userbotirbek2702@gmail.com>
2024-01-08 22:38:24 +0500

    newcode

--- index.js
+++ index.js
@@ -3,3 +3,20 @@
 
 // console.log(UserDate.User);
 // UserDate.userLogger() 
+
+
+const http = require("http")
+
+const server = http.createServer((request, response) =>{
+    //request - so'rov
+    //response - javob
+    console.log(request.url);
+
+    response.write("<h1><sapn style='color:red;'>Hello world</span></h1>")
+    response.end()
+})
+ 
+server.listen(5000, () =>{
+    console.log("Server has been started on port: 5000");
+})
+
