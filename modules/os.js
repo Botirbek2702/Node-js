076a8c91319f760c26d9e5312774ec72da2b9d0d
Botirbek2702 <userbotirbek2702@gmail.com>
2024-01-08 17:11:41 +0500

    new Node

---
+++ modules/os.js
@@ -0,0 +1,20 @@
+const os = require("os")
+  
+//platform
+// console.log(os.platform());
+
+//infarmatsiya
+//console.log(os.cpus());
+
+//architecture
+//console.log(os.arch());
+
+//free memoru
+//console.log(os.freemem());
+
+//total memory
+//console.log(os.totalmem());
+
+//entry point
+//console.log(os.homedir());
+
