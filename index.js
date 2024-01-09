9d0d9efcc34fb1dcb67a5f18c6d8724720e28736
Botirbek2702 <userbotirbek2702@gmail.com>
2024-01-09 14:30:07 +0500

    newcode

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const http = require("http")
+const fs = require("fs")
+const path = require("path")
+const { isUtf8 } = require("buffer")
 
 const server = http.createServer((req, res) =>{
     if(req.method === "GET"){
         res.writeHead(200 , {'Content-Type' : 'text/html'})
 
-        res.end(`
-        <h2>Hello wrold</h2>
-        <form method = "post" action="/"> 
-            <input name="name" placeholder="Enter your name" />
-            <button type="submit">Send name</button>
-        </form>`
-        )
 
-
+        if(req.url==="/"){
+            fs.readFile(path.join(__dirname, "templetse" , "index.html") , "utf-8" , (err,content) => {
+                if (err) throw err
+                res.end(content)
+            })
+        }else if(req.url==="/about"){
+            fs.readFile(path.join(__dirname, "templetse" , "about.html") , "utf-8" , (err,content) => {
+                if(err) throw err
+                res.end(content)
+            })
+        } else if(req.url === "/contact"){
+            fs.readFile(path.join(__dirname, "templetse" , "contact.html") , "utf-8" , (err,content) => {
+                if(err) throw err
+                res.end(content)
+            })
+         }
+    
+    
+    
     }else if (req.method==="POST"){
         const body = []
         res.writeHead(200, {"Content-Type" : "text-html; charset= utf-8"})
